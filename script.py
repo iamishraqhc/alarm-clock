df49c673f6798ea26b74bce86b310baf5bbf9c81
Ishraq Haider Chowdhury <ishraq-haider.chowdhury@stud.uni-bamberg.de>
2021-08-12 22:19:09 +0200

    alarm clock made

---
+++ script.py
@@ -0,0 +1,15 @@
+import datetime
+from playsound import playsound
+
+hour = int(input("Enter hour: "))
+minute = int(input("Enter minute: "))
+am_pm = input("am/pm: ")
+
+if am_pm == "pm":
+    hour = hour + 12
+    
+while True:
+    if hour == datetime.datetime.now().hour and minute == datetime.datetime.now().minute:
+        print("Alarm ON!")
+        playsound("alarm.mp3")
+        break
